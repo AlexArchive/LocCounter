cec7924a2222bf7ab9f8a185b58d5bb2c5a4c818
ByteBlast <byteblast@gmail.com>
2014-09-19 10:29:38 +0100

    Support for project aggregation.

--- LineCounter.fsx
+++ LineCounter.fsx
@@ -1,1 +1,17 @@
-﻿
+﻿open System
+open System.IO
+open System.Text.RegularExpressions
+
+Console.Write "Enter solution path: "
+let solutionPath = Console.ReadLine()
+let solutionDirPath = Path.GetDirectoryName(solutionPath)
+
+let solutionText = File.ReadAllText(solutionPath)
+let projectPaths  = 
+    Regex.Matches(solutionText, "Project.+?, \"(.+?\.csproj)")
+    |> Seq.cast<Match>
+    |> Seq.map (fun m -> Path.Combine(solutionDirPath, m.Groups.[1].Value))
+
+projectPaths
+|> Seq.iter (printfn "%s")
+Console.ReadKey() |> ignore
